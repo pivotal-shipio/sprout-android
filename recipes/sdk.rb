f66b483b06043585143892318c73f73ea7695718
Eric Black and Gregg Van Hove <pair+eric@ship.io+gvanhove@pivotallabs.com>
2014-11-17 17:39:06 -0800

    Install everything current for us.

--- recipes/sdk.rb
+++ recipes/sdk.rb
@@ -1,16 +1,7 @@
 package 'android-sdk'
 
-filters = %w(
-  build-tools-19.0.3
-  platform-tools
-  android-19
-  addon-google_apis-google-19
-  extra-android-m2repository
-  extra-android-support
-)
-
 execute 'update-sdk-system-tools-and-platforms' do
-  command "echo y | android update sdk --no-ui --filter #{filters.join(',')}"
+  command "echo y | android update sdk --no-ui"
 end
 
 atom_system_image_package_id = node['sprout']['android']['atom_system_image_package_id']
